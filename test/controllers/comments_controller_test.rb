6a2645b9f3704689213662eb09148c8fab60767a
esa0624 <e_chen@coloradocollege.edu>
2025-10-28 18:01:19 -0400

    delete rails test

--- test/controllers/comments_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class CommentsControllerTest < ActionDispatch::IntegrationTest
-  test "placeholder" do
-    skip "Rails default scaffold tests replaced by RSpec"
-  end
-end
--- test/controllers/posts_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class PostsControllerTest < ActionDispatch::IntegrationTest
-  test "placeholder" do
-    skip "Rails default scaffold tests replaced by RSpec"
-  end
-end
